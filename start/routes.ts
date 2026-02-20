bb331aac7a9683b13a69e5898ed9a30d885ed107
AnaMorales4 <alt.anamor4@gmail.com>
2026-02-20 18:12:14 -0500

    json de postman

--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,9 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import fs from 'fs'
+import { join } from 'path'
+
 
 const UsuariosController = () => import('../app/controllers/usuarios_controller.js')
 const ToursController= () => import('../app/controllers/tours_controller.js')
@@ -27,3 +30,11 @@ router.resource('tiquetes',TiquetesController)
 router.get('misTiquetes/:usuario_id', [TiquetesController, 'misTiquetes'])
 
 router.post('login', [UsuariosController, 'login'])
+
+router.get('/postman', async ({response}) => {
+  const filePath = join(process.cwd(), 'docs/ColombiaTour.postman_collection.json')
+    const file = fs.readFileSync(filePath, 'utf-8')
+
+  response.header('Content-Type', 'application/json')
+  return file
+})
